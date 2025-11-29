c48be130d05246d82decebffe22100a2786d080c
Gayoma <Gayoma@GAYOMAYUSI>
2025-11-29 18:18:39 +0800

    Add project files.

---
+++ ClubRegistrationQuery.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using System.Windows.Forms;
+using System.Net.Cache;
+using System.Security.Policy;
+
+namespace _07LaboratoryExercise
+{
+    class ClubRegistrationQuery
+    {
+        // Adds #5
+        private SqlConnection sqlConnect = new SqlConnection(connectionString);
+        private SqlCommand sqlCommand;
+        private SqlDataAdapter sqlAdapater;
+        private SqlDataReader sqlReader;
+
+        // Dili diay e static ang variable para matawag didto sa FrmClubRegistration
+        public DataTable dataTable = new DataTable();
+        public BindingSource bindingSource = new BindingSource();
+
+        // adds #5 b. establish local connection
+        private static string connectionString = "Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\CLAB1-\\Documents\\07LaboratoryExercise\\ClubDB.mdf;Integrated Security=True;";
+      
+        public string _FirstName, _MiddleName, _LastName, _Gender, _Program;
+        public int _Age;
+
+        // Initialize DataTable and DataSource (kulang? Hmmm)
+
+        // adds #7
+        public void DisplayList()
+        {
+            // dapat sa constructor class ni(kanang naa sa taas) pero ambot nganong dili ma invoke/tawag. Maong dri nlng ibutang
+            new SqlConnection(connectionString);
+
+            // adds #7 a.
+            string ViewClubMembers = "SELECT StudentId, FirstName, MiddleName, LastName, Age, Gender, Program FROM ClubMembers";
+
+            // adds #7 b.
+            sqlAdapater = new SqlDataAdapter(ViewClubMembers, connectionString);
+
+            // adds #7 c.
+            dataTable.Clear();
+            sqlAdapater.Fill(dataTable);
+            bindingSource.DataSource = dataTable; 
+        }
+
+        // adds #8
+        public bool RegisterStudent(int ID, long StudentID, string FirstName, string
+            MiddleName, string LastName, int Age, string Gender, string Program)
+        {
+            sqlCommand = new SqlCommand("INSERT INTO ClubMembers VALUES(@ID, @StudentID, @FirstName, @MiddleName, @LastName, @Age, @Gender, @Program)", sqlConnect);
+            sqlCommand.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            sqlCommand.Parameters.Add("@RegistrationID", SqlDbType.BigInt).Value = StudentID;
+            sqlCommand.Parameters.Add("@StudentID", SqlDbType.VarChar).Value = StudentID;
+            sqlCommand.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = FirstName;
+            sqlCommand.Parameters.Add("@MiddleName", SqlDbType.VarChar).Value = MiddleName;
+            sqlCommand.Parameters.Add("@LastName", SqlDbType.VarChar).Value = LastName;
+            sqlCommand.Parameters.Add("@Age", SqlDbType.Int).Value = Age;
+            sqlCommand.Parameters.Add("@Gender", SqlDbType.VarChar).Value = Gender;
+            sqlCommand.Parameters.Add("@Program", SqlDbType.VarChar).Value = Program;
+
+            sqlConnect.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConnect.Close();
+
+            return true; // Dapat ka mag return og value so return true 
+
+        }
+
+        public void IdSelect(ComboBox cb)
+        {
+            string selectId = "SELECT StudentId FROM ClubMembers ";
+            sqlCommand = new SqlCommand(selectId, sqlConnect);
+            sqlCommand.CommandText = selectId;
+            sqlConnect.Open();
+            sqlReader = sqlCommand.ExecuteReader();
+            while (sqlReader.Read())
+            {
+                cb.Items.Add(sqlReader["StudentId"].ToString());
+            }
+            sqlConnect.Close();
+
+        }
+
+        public void AutoSearch(TextBox tbFirstName, TextBox tbMiddleName, TextBox tbLastName, TextBox tbAge, ComboBox cbGender, ComboBox cbProgram, int ID)
+        {
+            string selectId = "SELECT FirstName,MiddleName,LastName,Age,Gender,Program FROM ClubMembers WHERE StudentId = @ID ";
+            sqlCommand = new SqlCommand(selectId, sqlConnect);
+            sqlCommand.Parameters.Add("@ID", SqlDbType.Int).Value = ID;
+            sqlCommand.CommandText = selectId;
+            sqlConnect.Open();
+            sqlReader = sqlCommand.ExecuteReader();
+            while (sqlReader.Read())
+            {
+                tbFirstName.Text = (sqlReader["FirstName"].ToString());
+                tbMiddleName.Text = (sqlReader["MiddleName"].ToString());
+                tbLastName.Text = (sqlReader["LastName"].ToString());
+                tbAge.Text = (sqlReader["Age"].ToString());
+                cbGender.Text = (sqlReader["Gender"].ToString());
+                cbProgram.Text = (sqlReader["Program"].ToString());
+            }
+            sqlConnect.Close();
+        }
+
+        public void UpdateStudent(long StudentID, string FirstName, string
+                                 MiddleName, string LastName, int Age, string Gender, string Program)
+        {
+            sqlCommand = new SqlCommand("UPDATE ClubMembers SET FirstName = @FirstName,MiddleName = @MiddleName,LastName= @LastName,Age= @Age,Gender= @Gender,Program= @Program WHERE StudentId = @StudentID", sqlConnect);
+
+            sqlCommand.Parameters.Add("@StudentID", SqlDbType.VarChar).Value = StudentID;
+            sqlCommand.Parameters.Add("@FirstName", SqlDbType.VarChar).Value = FirstName;
+            sqlCommand.Parameters.Add("@MiddleName", SqlDbType.VarChar).Value = MiddleName;
+            sqlCommand.Parameters.Add("@LastName", SqlDbType.VarChar).Value = LastName;
+            sqlCommand.Parameters.Add("@Age", SqlDbType.Int).Value = Age;
+            sqlCommand.Parameters.Add("@Gender", SqlDbType.VarChar).Value = Gender;
+            sqlCommand.Parameters.Add("@Program", SqlDbType.VarChar).Value = Program;
+            sqlConnect.Open();
+            sqlCommand.ExecuteNonQuery();
+            sqlConnect.Close();
+        }
+
+
+    }
+
+
+}
---
+++ FrmClubRegistration.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _07LaboratoryExercise
+{
+    public partial class FrmClubRegistration : Form
+    {
+
+        // adds #9 
+        private ClubRegistrationQuery clubRegistrationQuery = new ClubRegistrationQuery();
+        private static int ID, Age, count;
+        private string FirstName, MiddleName, LastName, Gender, Program;
+        private long StudentID;
+
+        public FrmClubRegistration()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRefresh_Click(object sender, EventArgs e)
+        {
+            RefreshListOfClubMembers();
+        }
+
+        private void FrmClubRegistration_Load(object sender, EventArgs e)
+        {
+            // add #11
+            RefreshListOfClubMembers();
+        }
+
+        // add #10
+        public void RefreshListOfClubMembers()
+        {
+
+            // add #10 a. 
+            clubRegistrationQuery.DisplayList();
+
+            // add #10 b.
+            dgvClubMembers.DataSource = clubRegistrationQuery.bindingSource;
+
+        }
+
+        // adds #12 
+        public int RegistrationID()
+        {
+            count++;
+            return count;
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            // adds #13
+            ID = RegistrationID();
+
+            StudentID = Convert.ToInt32(txtStudentID.Text);
+
+            FirstName = txtFirstName.Text;
+            MiddleName = txtMiddleName.Text;
+            LastName = txtLastName.Text;
+
+            Gender = cbGender.Text;
+            Program = cbProgram.Text;
+
+            Age = Convert.ToInt32(txtAge.Text);
+
+
+            clubRegistrationQuery.RegisterStudent(ID, StudentID, FirstName, MiddleName, LastName, Age, Gender, Program);
+            RegistrationID();
+
+            // adds #14
+
+        }
+
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnUpdate_Click(object sender, EventArgs e)
+        {
+            FrmUpdateMember frmUpdateMember = new FrmUpdateMember();
+            frmUpdateMember.Show();
+        }
+
+       
+       
+    }
+}
